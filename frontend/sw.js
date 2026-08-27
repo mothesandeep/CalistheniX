8c6888340b0c1edef580c73c6601620c57028e26
mothesandeep <mothesandeep445@gmail.com>
2026-08-27 10:02:25 +0530

    feat(frontend): modularize CSS into variables, base, layout, and components stylesheets

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,8 +1,12 @@
-const CACHE_NAME = 'calisthenix-v2.4';
+const CACHE_NAME = 'calisthenix-v2.5';
 const STATIC_ASSETS = [
   './',
   './index.html',
   './style.css',
+  './css/variables.css',
+  './css/base.css',
+  './css/layout.css',
+  './css/components.css',
   './js/state.js',
   './js/api.js',
   './js/views/home.js',
